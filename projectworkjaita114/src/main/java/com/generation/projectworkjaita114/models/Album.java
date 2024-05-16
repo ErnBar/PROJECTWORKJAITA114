e5e871949daf3f539faf2b9012281708863e967f
LeoNiccolini <167442173+LeoNiccolini@users.noreply.github.com>
2024-05-16 14:40:26 +0200

    Update Album.java

--- projectworkjaita114/src/main/java/com/generation/projectworkjaita114/models/Album.java
+++ projectworkjaita114/src/main/java/com/generation/projectworkjaita114/models/Album.java
@@ -1,5 +1,27 @@
 package com.generation.projectworkjaita114.models;
 
-public class Album {
+import java.util.ArrayList;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+@Entity
+@Data
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titoloAlbum;
+    private int annoPubblicazione;
+    @ManyToOne
+    @JoinColumn(name = "id_artista")
+    private Artista artista;
+    
+    @OneToMany(mappedBy = "album")
+    private ArrayList<Canzone> canzone;
 }
