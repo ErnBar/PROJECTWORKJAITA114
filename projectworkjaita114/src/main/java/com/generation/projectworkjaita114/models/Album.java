d8f337927d8227e7f62257beddbe1468b6c88992
ErnBar <ernest23.barri@gmail.com>
2024-05-16 14:43:53 +0200

    Update Album.java

--- projectworkjaita114/src/main/java/com/generation/projectworkjaita114/models/Album.java
+++ projectworkjaita114/src/main/java/com/generation/projectworkjaita114/models/Album.java
@@ -2,6 +2,7 @@ package com.generation.projectworkjaita114.models;
 
 import java.util.ArrayList;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -16,8 +17,10 @@ public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String titoloAlbum;
-    private int annoPubblicazione;
+    @Column(name = "titolo_album")
+    private String titolo_album;
+    @Column(name = "anno_pubblicazione")
+    private int anno_pubblicazione;
     @ManyToOne
     @JoinColumn(name = "id_artista")
     private Artista artista;
