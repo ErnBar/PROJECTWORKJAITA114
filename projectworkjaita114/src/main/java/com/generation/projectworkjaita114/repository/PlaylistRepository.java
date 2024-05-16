39a41b39bffef0c7dc96058521ea7c13250ddb6d
GiorgiaDiGiacomo <giorgiadigiacomo30@gmail.com>
2024-05-16 15:52:52 +0200

    Commit PlaylistRepository

---
+++ projectworkjaita114/src/main/java/com/generation/projectworkjaita114/repository/PlaylistRepository.java
@@ -0,0 +1,13 @@
+package com.generation.projectworkjaita114.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.generation.projectworkjaita114.models.Playlist;
+
+public interface PlaylistRepository extends CrudRepository<Playlist, Long>{
+
+
+
+
+    
+} 
