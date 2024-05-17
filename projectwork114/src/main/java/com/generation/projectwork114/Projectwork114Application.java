0298b24232cfeb1f0ff1b54ea7e566af4b15a4b4
StefanoCastelletti <stefacastelletti@gmail.com>
2024-05-17 11:36:45 +0200

    Repo

---
+++ projectwork114/src/main/java/com/generation/projectwork114/Projectwork114Application.java
@@ -0,0 +1,13 @@
+package com.generation.projectwork114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectwork114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectwork114Application.class, args);
+	}
+
+}
---
+++ projectwork114/src/test/java/com/generation/projectwork114/Projectwork114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projectwork114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectwork114ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
