6f261df214e8166c3f709879497fe1733d79b1ee
ErnBar <ernest23.barri@gmail.com>
2024-05-21 09:17:28 +0200

    delete

--- projectwork114/src/main/java/com/generation/projectwork114/database/IDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.generation.projectwork114.database;
-
-import java.util.List;
-import java.util.Map;
-
-public interface IDatabase {
-    int executeUpdate(String query,String...params);
-    List<Map<String,String>> executeQuery(String query,String...params);
-}
--- projectwork114/src/main/java/com/generation/projectwork114/models/Entity.java
+++
@@ -1,9 +0,0 @@
-package com.generation.projectwork114.models;
-
-import lombok.Data;
-
-@Data
-public class Entity {
-    
-    private Long id;
-}
